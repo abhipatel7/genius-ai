09fedfbeb4ceca4acf193c294e12545156d78a60
Abhishek Patel <abhipatel7420@gmail.com>
2023-08-06 20:46:44 +0530

    feat: add stripe for genius + subscription

--- src/components/pro-modal.tsx
+++ src/components/pro-modal.tsx
@@ -1,5 +1,9 @@
 'use client';
 
+import { Check, Zap } from 'lucide-react';
+import axios from 'axios';
+import { useState } from 'react';
+
 import {
   Dialog,
   DialogContent,
@@ -13,11 +17,24 @@ import { Badge } from '@/components/ui/badge';
 import { tools } from '@/constants';
 import { Card } from '@/components/ui/card';
 import { cn } from '@/lib/utils';
-import { Check, Zap } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 
 export default function ProModal() {
   const proModal = useProModal();
+  const [loading, setLoading] = useState(false);
+
+  const onSubscribe = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get('/api/stripe');
+
+      window.location.href = response.data.url;
+    } catch (error) {
+      console.log({ error }, 'STRIPE_CLIENT_ERROR');
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
@@ -49,7 +66,12 @@ export default function ProModal() {
           </DialogDescription>
         </DialogHeader>
         <DialogFooter>
-          <Button size="lg" variant="premium" className="w-full">
+          <Button
+            onClick={onSubscribe}
+            size="lg"
+            variant="premium"
+            className="w-full"
+          >
             Upgrade <Zap className="w-4 h-4 ml-2 fill-white" />
           </Button>
         </DialogFooter>
